066b06c43c85f415680650580aa24ca911997bbb
Truong Do <106587727+ttruongg@users.noreply.github.com>
2025-04-21 10:25:31 +0700

    fix: product missing field
    Adding two new fields, createOn and lastUpdatedOn to Product Entity

--- back-end/src/main/java/com/assignment/ecommerce_rookie/EcommerceRookieApplication.java
+++ back-end/src/main/java/com/assignment/ecommerce_rookie/EcommerceRookieApplication.java
@@ -2,12 +2,14 @@ package com.assignment.ecommerce_rookie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class EcommerceRookieApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EcommerceRookieApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceRookieApplication.class, args);
+    }
 
 }
