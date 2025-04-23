563a5d00f7345cc233db06b716890fdcf3ae2839
ThanhhTruongg1901 <thanhtruong1901@gmail.com>
2025-04-23 13:59:03 +0700

    feat: creating and configuring dependencies

---
+++ front-end/src/App.jsx
@@ -0,0 +1,11 @@
+import { BiAdjust } from "react-icons/bi";
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-800 text-white text-2xl font-bold">
+      Welcome <BiAdjust />
+    </div>
+  );
+}
+
+export default App;
