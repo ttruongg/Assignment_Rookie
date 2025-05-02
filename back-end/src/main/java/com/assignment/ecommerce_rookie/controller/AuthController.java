254c5c19611038435e5a64231720e25f7b42eed0
Truong Do <106587727+ttruongg@users.noreply.github.com>
2025-05-02 12:44:40 +0700

    feat: Implement product filtering and search
    * feat: implment product specification and adhere to naming conventions

--- back-end/src/main/java/com/assignment/ecommerce_rookie/controller/AuthController.java
+++ back-end/src/main/java/com/assignment/ecommerce_rookie/controller/AuthController.java
@@ -2,7 +2,7 @@ package com.assignment.ecommerce_rookie.controller;
 
 import com.assignment.ecommerce_rookie.security.request.LoginRequest;
 import com.assignment.ecommerce_rookie.security.request.SignUpRequest;
-import com.assignment.ecommerce_rookie.service.impl.AuthService;
+import com.assignment.ecommerce_rookie.service.impl.AuthServiceImpl;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,27 +15,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
+  
     @Autowired
-    private AuthService authService;
+    private AuthServiceImpl authServiceImpl;
 
     @PostMapping("/signin")
     public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
-        return authService.login(loginRequest);
+        return authServiceImpl.login(loginRequest);
+
     }
 
     @PostMapping("/refresh-token")
     public ResponseEntity<?> refreshAccessToken(HttpServletRequest request) {
-        return authService.refreshAccessToken(request);
+        return authServiceImpl.refreshAccessToken(request);
+
     }
 
     @PostMapping("/signup")
     public ResponseEntity<?> registerUser(@Valid @RequestBody SignUpRequest signUpRequest) {
-        return authService.signup(signUpRequest);
+        return authServiceImpl.signup(signUpRequest);
+
     }
 
     @PostMapping("/logout")
     public ResponseEntity<?> logout() {
-        return authService.logout();
+        return authServiceImpl.logout();
     }
 
 
--- back-end/src/main/java/com/assignment/ecommerce_rookie/controller/ProductController.java
+++ back-end/src/main/java/com/assignment/ecommerce_rookie/controller/ProductController.java
@@ -19,12 +19,14 @@ public class ProductController {
 
     @GetMapping("/public/products")
     public ResponseEntity<ProductResponse> getAllProduct(
+            @RequestParam(name = "keyword", required = false) String keyword,
+            @RequestParam(name = "category", required = false) String category,
             @RequestParam(defaultValue = AppConstants.PAGE_NUMBER) int pageNumber,
             @RequestParam(defaultValue = AppConstants.PAGE_SIZE) int pageSize,
             @RequestParam(defaultValue = AppConstants.SORT_PRODUCTS_BY) String sortBy,
             @RequestParam(defaultValue = AppConstants.SORT_ORDER) String sortOrder
     ) {
-        ProductResponse products = productService.getAllProducts(pageNumber, pageSize, sortBy, sortOrder);
+        ProductResponse products = productService.getAllProducts(pageNumber, pageSize, sortBy, sortOrder, keyword, category);
         return new ResponseEntity<>(products, HttpStatus.OK);
     }
 
@@ -38,7 +40,7 @@ public class ProductController {
     }
 
 
-    @GetMapping("public/{categoryId}/products")
+    @GetMapping("/public/{categoryId}/products")
     public ResponseEntity<ProductResponse> getProductByCategory(
             @PathVariable(name = "categoryId") Long categoryId,
             @RequestParam(defaultValue = AppConstants.PAGE_NUMBER) int pageNumber,
