b381319458bf0bdfbf039d44b4672a04f3dc1a12
ThanhhTruongg1901 <thanhtruong1901@gmail.com>
2025-05-06 16:09:56 +0700

    feat: homepage UI

---
+++ front-end/src/utils/index.js
@@ -0,0 +1,6 @@
+export const bannerLists = [
+    { id: 1, image: 'https://cdn2.cellphones.com.vn/insecure/rs:fill:690:300/q:90/plain/https://dashboard.cellphones.com.vn/storage/m4-len-doi-tang-airpods-4.jpg' },
+    { id: 2, image: 'https://cdn2.cellphones.com.vn/insecure/rs:fill:690:300/q:90/plain/https://dashboard.cellphones.com.vn/storage/dinh-gia-home.jpg' },
+    { id: 3, image: 'https://cdn2.cellphones.com.vn/insecure/rs:fill:690:300/q:90/plain/https://dashboard.cellphones.com.vn/storage/iphone-16-pro-max-thu-cu-moi-home.jpg' },
+  ];
+  
